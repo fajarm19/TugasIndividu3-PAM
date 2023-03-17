5c2de54f30fb3e8a2636a74ad957b3572c30b226
fajarm19 <fajar.120140136@student.itera.ac.id>
2023-03-17 22:31:51 +0700

    Inital Commit

---
+++ src/Data.jsx
@@ -0,0 +1,110 @@
+export const Jadwal = [
+  {
+    jadwal_id: "1",
+    bandara_kode_keberangkatan: "CKG",
+    bandara_kode_tujuan: "KNO",
+    maskapai_id: "GRD",
+  },
+  {
+    jadwal_id: "2",
+    bandara_kode_keberangkatan: "CKG",
+    bandara_kode_tujuan: "KNO",
+    maskapai_id: "CTL",
+  },
+  {
+    jadwal_id: "3",
+    bandara_kode_keberangkatan: "CKG",
+    bandara_kode_tujuan: "KNO",
+    maskapai_id: "LNR",
+  },
+  {
+    jadwal_id: "4",
+    bandara_kode_keberangkatan: "KNO",
+    bandara_kode_tujuan: "TKG",
+    maskapai_id: "GRD",
+  },
+  {
+    jadwal_id: "5",
+    bandara_kode_keberangkatan: "KNO",
+    bandara_kode_tujuan: "TKG",
+    maskapai_id: "CTL",
+  },
+  {
+    jadwal_id: "6",
+    bandara_kode_keberangkatan: "KNO",
+    bandara_kode_tujuan: "TKG",
+    maskapai_id: "LNR",
+  },
+  {
+    jadwal_id: "7",
+    bandara_kode_keberangkatan: "TKG",
+    bandara_kode_tujuan: "CKG",
+    maskapai_id: "GRD",
+  },
+  {
+    jadwal_id: "8",
+    bandara_kode_keberangkatan: "TKG",
+    bandara_kode_tujuan: "CKG",
+    maskapai_id: "CTL",
+  },
+  {
+    jadwal_id: "9",
+    bandara_kode_keberangkatan: "TKG",
+    bandara_kode_tujuan: "CKG",
+    maskapai_id: "LNR",
+  },
+  {
+    jadwal_id: "10",
+    bandara_kode_keberangkatan: "TKG",
+    bandara_kode_tujuan: "CKG",
+    maskapai_id: "GRD",
+  },
+  {
+    jadwal_id: "11",
+    bandara_kode_keberangkatan: "TKG",
+    bandara_kode_tujuan: "KNO",
+    maskapai_id: "LNR",
+  },
+  {
+    jadwal_id: "12",
+    bandara_kode_keberangkatan: "CKG",
+    bandara_kode_tujuan: "TKG",
+    maskapai_id: "CTL",
+  },
+  {
+    jadwal_id: "13",
+    bandara_kode_keberangkatan: "KNO",
+    bandara_kode_tujuan: "CKG",
+    maskapai_id: "GRD",
+  },
+];
+
+export const Maskapai = [
+  {
+    maskapai_id: "GRD",
+    maskapai_name: "Garuda",
+  },
+  {
+    maskapai_id: "CTL",
+    maskapai_name: "Citilink",
+  },
+  {
+    maskapai_id: "LNR",
+    maskapai_name: "Lion Air",
+  },
+];
+
+export const Bandara = [
+  {
+    bandara_kode: "CKG",
+    bandara_nama: "Soekarno Hatta",
+  },
+  {
+    bandara_kode: "KNO",
+    bandara_nama: "Kualanamu",
+  },
+  {
+    bandara_kode: "TKG",
+    bandara_nama: "Raden Inten II",
+  },
+];
